cb288d0bac29875e3f2df3437ccb4f53e69b3e6f
Denis K <denis.kolosov@gmail.com>
2015-09-13 23:51:22 +0300

    Added -y option to apt-get install, fixed a bug in cd

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@
 
 sudo apt-get update
 
-sudo apt-get install build-essential
-sudo apt-get install libsqlite3-dev
-sudo apt-get install sqlite3
-sudo apt-get install bzip2 libbz2-dev
+sudo apt-get -y install build-essential
+sudo apt-get -y install libsqlite3-dev
+sudo apt-get -y install sqlite3
+sudo apt-get -y install bzip2 libbz2-dev
 
 wget https://www.python.org/ftp/python/3.4.3/Python-3.4.3.tar.xz
 tar xJf ./Python-3.4.3.tar.xz
@@ -13,9 +13,9 @@ cd ./Python-3.4.3
 ./configure --prefix=/opt/python3.4
 make && sudo make install
 
-sudo apt-get install python3-pip
+sudo apt-get -y install python3-pip
 
-cd polyclinic/
+cd ../polyclinic/
 sudo pip3 install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
